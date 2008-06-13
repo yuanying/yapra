ebee0cfcd1ba81b6279393ae422d752c20e6d9a5
yuanying <yuanying@bfab7902-dd0e-0410-bd88-c35cf44bdc1f>
2008-06-13 07:15:23 +0000

    fix bug for unescaped value for instance_eval.
    git-svn-id: http://www.fraction.jp/repos/yapra/trunk@746 bfab7902-dd0e-0410-bd88-c35cf44bdc1f

--- lib/yapra/plugin/feed_item_operator.rb
+++ lib/yapra/plugin/feed_item_operator.rb
@@ -1,16 +1,21 @@
 require 'yapra/plugin'
 
 module Yapra::Plugin::FeedItemOperator
+  LOCAL_VAL_RE = /[a-z_][0-9A-Za-z_]/
+  
   def set_attribute_to item, k, value
-    if item.respond_to?("#{k}=")
-      item.__send__("#{k}=", value)
-    else
+    raise NameError unless LOCAL_VAL_RE =~ k
+    unless item.respond_to?("#{k}=")
       item.instance_eval %Q{
-        @#{k} = "#{value}"
         def #{k}
           @#{k}
         end
+        def #{k}= v
+          @#{k} = v
+        end
       }, __FILE__, __LINE__
+      
     end
+    item.__send__("#{k}=", value)
   end
 end
